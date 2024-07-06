0020fb80ae839d4d7a1a4d842449def72cb7095c
Antony Wahome <155991781+tonywahome@users.noreply.github.com>
2024-07-06 18:07:12 +0200

    debugs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+        data = response.json()["data"]
+        return data["subscribers"]
+    else:
+        return 0
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-function that queries the 'Reddit API'
-and prints the titles of the first 10 hot posts listed for a given subreddit.
-"""
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://oauth.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'MyBot/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
